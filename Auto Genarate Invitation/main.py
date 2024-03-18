cd0b5a608bfa6225aef45ff47116dc3d1eea36e9
Harini Rodrigo <77738765+HARINIRODRIGO@users.noreply.github.com>
2024-03-18 20:35:44 +0530

    Update main.py

--- Auto Genarate Invitation/main.py
+++ Auto Genarate Invitation/main.py
@@ -3,4 +3,4 @@ invitor_name = names.read()
 name = invitor_name.split("\n")
 for i in name:
     letter = open(f"Output/ReadyToSend/{i}_letter.txt", "w")
-    letter.write(f"Dear {i},\n\nYou are invited to my birthday this Saturday.\n\nHope you can make it!\n\nAngela")
+    letter.write(f"Dear {i},\n\nYou are invited to my birthday this Saturday.\n\nHope you can make it!\n\Harini")
